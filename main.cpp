ca0f8239427976280651b5a2575ffa6365ff0dea
maki0905 <k022g0059@g.neec.ac.jp>
2023-10-16 12:25:35 +0900

    01_04

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <functional>
+#include <stdlib.h>
+#include <Windows.h>
+
+int main(int argc, const char* argv[]) {
+	
+	std::function<void(int)> result = [] (int choice){
+		int dice = rand() % 6 + 1;
+		if (dice % 2 == choice) {
+			[=]() {printf("当たり!! サイコロの目 : %d\n", dice); }();
+		}
+		else {
+			[=]() {printf("はずれ;;  サイコロの目 : %d\n", dice); }();
+		}
+	};
+	std::function<void(int)> judge = [=] (int choice){
+		Sleep(3 * 1000);
+		result(choice);
+	};
+	int choice = 0;
+	while (choice != 9) {
+		[]() {printf("半(奇数)なら1を丁(偶数)なら0を入力してください\n"); }();
+		[]() {printf("やめる場合は9を押してください\n"); }();
+		[&]() {scanf_s("%d", &choice); }();
+		if (choice < 2) {
+			judge(choice);
+		}
+	}
+
+
+	return 0;
+}
